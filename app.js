822f011b337bfa476df8bec632774273f3a0c72c
Ziyu Wang <odduid@gmail.com>
2016-10-22 18:48:25 +1100

    add filter input and loading state

--- app.js
+++ app.js
@@ -5,23 +5,39 @@
 angular.module('app', [])
   .constant('d3', d3)
   .constant('_', _)
-  .controller('AppController', ['$q', 'd3', AppController]);
+  .controller('AppController', ['$scope', '$q', 'd3', 'dataHelper', AppController]);
 
-function AppController($q, d3) {
+function AppController($scope, $q, d3, dataHelper) {
 
   var DATA_URL = 'https://s3-ap-southeast-2.amazonaws.com/glow-dev-assets/flight-data-gz.csv',
     app = this;
 
+  app.destination = '';
+  app.loading = false;
+
   bootstrap();
 
   function bootstrap() {
+    $scope.$watch('app.destination', function(destination) {
+      if (destination) {
+        app.filteredData = dataHelper.filterBy(app.data, 'destination', destination)
+      } else {
+        app.filteredData = app.data;
+      }
+    });
+
     return getData(DATA_URL)
       .then(function(data) {
-        app.data = data;
+        app.data = app.filteredData = data;
+      })
+      .finally(function() {
+        app.loading = false;
       });
   }
 
   function getData(url) {
+    app.loading = true;
+
     return $q(function(resolve, reject) {
       d3.csv(url, function(error, data) {
         if (error) {
